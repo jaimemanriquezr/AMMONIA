139c4b45cd1ba77d49f76a46887154ec92a67439
Jaime <jr.manriquezr@gmail.com>
2026-03-05 23:04:13 +0100

    (fix): wrong location for LundMPCModel.mat

--- example.m
+++ example.m
@@ -1,13 +1,10 @@
-clc; clear; restoredefaultpath; 
-initpath; 
+clc; clear; restoredefaultpath;
+initpath;
 import MPC.*;
 
 filter = addGridPoints(SandFilter(), 200);
-model = load("./LundMPCModel.mat").model;
-state = State(filter, model);
+model = load("./data/LundMPCModel.mat").model;
+inflow = [1e-4, 2e-4, 0, 0, 1e-5, 1e-6, 1e-9, 0, 1e-5];
 
-tic
-results = simulate(state, ...
-    InflowConcentrations=[1e-3, 1e-3, 0, 1e-5, 1e-2, 1e-2, 1e-5, 0, 1e-4] / 10, ...
-    SimulationTime=1.0, TimeStep=1e-6);
-toc
+filterState = State(filter, model);
+results = filterState.simulate(InflowConcentrations=inflow, SimulationTime=10.0);
